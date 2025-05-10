c99f95ac9f9a7af74c0fb07bc09c316a35a2a39c
GG1427 <24f1001824@ds.study.iitm.ac.in>
2025-05-10 12:32:09 +0530

    Update user.py

--- api/user.py
+++ api/user.py
@@ -1,28 +1,26 @@
 from http.server import BaseHTTPRequestHandler
- 
+
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-	    
-		response_body = "Guess who's back"
-		path = self.path
-		
+        response_body = "Guess who's back"
+        path = self.path
+
         if path.startswith("/api?"):
             query_string = path.split("?", 1)[1]
             params = {}
 
             # Manual parsing of query string
             for pair in query_string.split("&"):
-                key, value = pair.split("=")
-                if key in params:
-                    params[key].append(value)
-                else:
-                    params[key] = [value]
+                if "=" in pair:
+                    key, value = pair.split("=", 1)
+                    if key in params:
+                        params[key].append(value)
+                    else:
+                        params[key] = [value]
 
             response_body = str(params)
-            
+
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
         self.wfile.write(response_body.encode())
-        
-       return
