67b6a6fe95cca4f6233fe9fe4612ffd30267b5f1
GG1427 <24f1001824@ds.study.iitm.ac.in>
2025-05-10 12:25:12 +0530

    Update user.py

--- api/user.py
+++ api/user.py
@@ -7,4 +7,19 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Content-type','text/plain')
         self.end_headers()
         self.wfile.write('Hello, world!!'.encode('utf-8'))
+        path = self.path
+
+        if path.startswith("/api?"):
+            query_string = path.split("?", 1)[1]
+            params = {}
+
+            # Manual parsing of query string
+            for pair in query_string.split("&"):
+                key, value = pair.split("=")
+                if key in params:
+                    params[key].append(value)
+                else:
+                    params[key] = [value]
+
+         self.wfile.write(str(params).encode('utf-8'))
         return
