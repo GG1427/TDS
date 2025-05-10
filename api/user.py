2806f9212a7ef866e91abaeb19ba4fff24b7c562
GG1427 <24f1001824@ds.study.iitm.ac.in>
2025-05-10 12:26:54 +0530

    Update user.py

--- api/user.py
+++ api/user.py
@@ -3,12 +3,6 @@ from http.server import BaseHTTPRequestHandler
 class handler(BaseHTTPRequestHandler):
  
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write('Hello, world!!'.encode('utf-8'))
-        path = self.path
-
         if path.startswith("/api?"):
             query_string = path.split("?", 1)[1]
             params = {}
@@ -22,4 +16,10 @@ class handler(BaseHTTPRequestHandler):
                     params[key] = [value]
 
             self.wfile.write(str(params).encode('utf-8'))
+            
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write('Hello, world!!'.encode('utf-8'))
+        path = self.path
         return
