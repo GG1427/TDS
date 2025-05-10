34375a83cff112b9c6f7edb9d57363706b0ccb59
GG1427 <24f1001824@ds.study.iitm.ac.in>
2025-05-10 12:34:24 +0530

    Update user.py

--- api/user.py
+++ api/user.py
@@ -1,10 +1,10 @@
 from http.server import BaseHTTPRequestHandler
-
+ 
 class handler(BaseHTTPRequestHandler):
+ 
     def do_GET(self):
-        path = self.path
-        response_body = "Guess who's back"
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
+        self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write(response_body.encode())
+        self.wfile.write('Hello, world!'.encode('utf-8'))
+        return
