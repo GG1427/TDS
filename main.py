840a3db6a637d5a4c7689f2eb9898ee7876cd652
GG1427 <24f1001824@ds.study.iitm.ac.in>
2025-05-10 13:01:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, json
 from urllib.parse import urlparse
 
 app = FastAPI()
@@ -7,4 +7,12 @@ app = FastAPI()
 async def read_api(request: Request):
     # Get the full URL from the request
     query_string = urlparse(str(request.url)).query
-    return {"query_string": query_string}
+    with open("data.json", "r") as file:
+        data = json.load(file)
+    name1 = query_string.split("&")[0].split("=")[1]
+    name2 = query_string.split("&")[1].split("=")[1]
+    li = []
+    li.append(data[name1])
+    li.append(data[name2])
+    
+    return {"marks": li}
