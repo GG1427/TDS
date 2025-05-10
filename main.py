6f33be4707da53f362dd86267bdc720824be4c1d
GG1427 <24f1001824@ds.study.iitm.ac.in>
2025-05-10 12:56:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from urllib.parse import urlparse
 
 app = FastAPI()
 
 @app.get("/api")
-def read_api(request: str):
-    query_string = urlparse(request).query
+async def read_api(request: Request):
+    # Get the full URL from the request
+    query_string = urlparse(str(request.url)).query
     return {"query_string": query_string}
