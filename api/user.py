fabe662785dde7107197ab252763f7912e3ad160
GG1427 <24f1001824@ds.study.iitm.ac.in>
2025-05-10 12:33:00 +0530

    Update user.py

--- api/user.py
+++ api/user.py
@@ -2,23 +2,25 @@ from http.server import BaseHTTPRequestHandler
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        response_body = "Guess who's back"
         path = self.path
+        response_body = "Guess who's back"
 
-        if path.startswith("/api?"):
-            query_string = path.split("?", 1)[1]
+        if path.startswith("/api"):
             params = {}
+            if "?" in path:
+                query_string = path.split("?", 1)[1]
 
-            # Manual parsing of query string
-            for pair in query_string.split("&"):
-                if "=" in pair:
-                    key, value = pair.split("=", 1)
-                    if key in params:
-                        params[key].append(value)
-                    else:
-                        params[key] = [value]
+                for pair in query_string.split("&"):
+                    if "=" in pair:
+                        key, value = pair.split("=", 1)
+                        if key in params:
+                            params[key].append(value)
+                        else:
+                            params[key] = [value]
 
-            response_body = str(params)
+                response_body = str(params)
+            else:
+                response_body = "No query parameters provided"
 
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
