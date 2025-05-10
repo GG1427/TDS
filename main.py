ccb17c154406ef3383b22c58a0732dc7a3906c5e
GG1427 <24f1001824@ds.study.iitm.ac.in>
2025-05-10 12:49:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-# main.py
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
+def read_root(name: str = None):
+    if name:
+        return {"name": name}
     return {"message": "Hello, World! Coming to you, from main.py"}
